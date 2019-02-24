4381cabeab26c4842796d3221942374abce7c322
David Yang <saluteyang@gmail.com>
2019-02-23 19:33:54 -0500

    added charting code

--- vw_test.py
+++ vw_test.py
@@ -21,6 +21,24 @@ from scipy.interpolate import interp1d
 from helpers import ENGLISH_STOP_WORDS, chunks, display_topics, \
     potential_stop_words, plotSentiment
 
+
+stop_words = stopwords.words('english')
+stop_words_v0 = stop_words + ["man", "men", "woman", "women", "mr", "mrs", "ramsay", "chapter"]
+stop_words_v1 = stop_words_v0 + ["macalister", "charles", "tansley", "minta", "paul", "rayley",
+                                  "beckwith", "william", "bankes", "carmichael", "mcnab", "husband",
+                                  "wife", "son", "girl", "nancy", "joseph", "mildred", "scott",
+                                  "augustus", "carrie", "mary", "damn"]
+stop_words_v2 = stop_words_v1 + ["felt", "thought", "wanted", "knew", "looked", "looking", "liked", "say", "came"]
+stop_words_v3 = stop_words_v2 + ["look", "come", "went", "saw", "things", "people", "time", "mind",
+                                  "way", "old", "thinking", "going", "lily", "james", "eyes", "cam",
+                                  "moment", "children", "thing", "room", "table", "let", "house", "hand",
+                                  "away", "father"]
+stop_words_v4 = stop_words_v3 + ["something", "little", "nothing", "never", "always", "suddenly", "still",
+                                 "even", "perhaps", "anything", "good", "well", "life", "light", "world",
+                                 "book", "day", "long", "word", "round", "great", "light", "together", "alone"]
+stop_words_v5 = stop_words_v4 + ["back", "year", "head", "tree", "whole", "rather", "quite", "half",
+                                 "window", "last", "much", "ever", "everything", "feeling"]
+
 # use different document segmentation
 # use pretrained sentiment model for the chapters of To the Lighthouse
 # use on a different text Mrs Dalloway?
@@ -66,10 +84,29 @@ vectorizer = TfidfVectorizer(stop_words=potential_stop_words2,
 doc_word = vectorizer.fit_transform(docs)
 doc_word.shape
 
+# np.savetxt('50_doc_word_out', doc_word.toarray(),  delimiter='\t')
+
 # NMF model
 nmf_model = NMF(3)
 doc_topic = nmf_model.fit_transform(doc_word)
 display_topics(nmf_model, vectorizer.get_feature_names(), 10)
+
+# np.savetxt('50_doc_out', doc_topic, delimiter='\t')
+
+# NMF model 2
+docs = [(' ').join(x) for x in chunks(text_clean, 5)]
+
+# custom stop words
+vectorizer = TfidfVectorizer(stop_words=potential_stop_words2,
+                             ngram_range=(1, 1),
+                             max_df=0.8)
+
+doc_word = vectorizer.fit_transform(docs)
+doc_word.shape
+
+nmf_model2 = NMF(4)
+doc_topic2 = nmf_model2.fit_transform(doc_word)
+display_topics(nmf_model2, vectorizer.get_feature_names(), 10)
 
 # sentiment analysis
 docs = [(' ').join(x) for x in chunks(text_clean, 10)]
@@ -98,6 +135,65 @@ for i in range(len(text_sent_ch)):
 
 plotSentiment(text_polar_ch, len(text_chap), save=True, savefile='text_sent_ch.png')
 
+from sklearn.manifold import TSNE
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import Label, LabelSet, ColumnDataSource
+from bokeh.io import output_notebook
+import seaborn as sns
+import matplotlib.colors as mcolors
+
+# Dominant topic number in each doc
+topic_num = np.argmax(doc_topic2, axis=1)
+
+# tSNE Dimension Reduction
+tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=.99, init='pca')
+tsne_nmf = tsne_model.fit_transform(doc_topic2)
+
+# Plot the Topic Clusters using Bokeh
+TOOLS = "hover,pan,wheel_zoom,box_zoom,reset,save"
+n_topics = 4
+mycolors = np.array([color for name, color in mcolors.TABLEAU_COLORS.items()])
+
+# create a df for tooltip assignment
+doc_topic_p = pd.DataFrame({'topic': topic_num,
+                            'topic_idx': range(1, doc_topic2.shape[0] + 1),
+                            'x': tsne_nmf[:, 0],
+                            'y': tsne_nmf[:, 1],
+                            'color': mycolors[topic_num]})
+
+plot = figure(title="t-SNE Clustering of {} NMF Topics".format(n_topics),
+              plot_width=900, plot_height=700, tools=TOOLS)
+plot.hover.tooltips = [
+    ("document topic", "@topic"),
+    ("document number", "@topic_idx")
+]
+source = ColumnDataSource(doc_topic_p)
+plot.scatter(x='x', y='y', color='color', source=source, size=10)
+show(plot)
+
+# try NMF on chapter numbering
+# split based on chapter numbering
+with open('txt_data/to_the_lighthouse_1927.txt') as f:
+    text = f.readlines()
+text = [''.join(text)][0]
+
+# split based on digits
+text_chap = re.compile('\w*\d\w*').split(text)[1:]  # remove first section header
+text_chap = [[word for word in simple_preprocess(str(doc))] for doc in text_chap]
+docs = [' '.join(x) for x in text_chap]
+
+vectorizer = TfidfVectorizer(stop_words=potential_stop_words2,
+                             ngram_range=(1, 1),
+                             max_df=0.9)
+doc_word = vectorizer.fit_transform(docs)
+doc_word.shape
+
+# NMF model
+nmf_model = NMF(3)
+doc_topic = nmf_model.fit_transform(doc_word)
+display_topics(nmf_model, vectorizer.get_feature_names(), 10)
+
+
 # remove sentiment outliers
 # text_polar_chx = text_polar_ch
 # text_polar_chx[1] = (text_polar_ch[0] + text_polar_ch[2])/2
@@ -114,6 +210,13 @@ plotSentiment(text_polar_ch, len(text_chap), save=True, savefile='text_sent_ch.p
 # Topic  3
 # lights, lost, flounder, grow, love, reason, necklace, bit, oneself, brooch
 
+#######
+# topic 0: surroundings, environment
+# topic 1: garbage
+# topic 2: male characters
+# topic 3: female characters
+#######
+
 # topic results (50 documents, 3 topics, 0.9 max_df)
 # Topic  0
 # lily, rose, people, life, saying, things, room, saw, love, eyes
@@ -122,15 +225,62 @@ plotSentiment(text_polar_ch, len(text_chap), save=True, savefile='text_sent_ch.p
 # Topic  2
 # house, night, summer, light, glass, bast, room, wall, left, long
 
+# topic results (43 documents, 3 topics, 0.9 max_df, by chapter)
+# Topic  0
+# said, did, lily, little, like, things, people, andrew, picture, eyes
+# Topic  1
+# james, cam, father, boat, like, lighthouse, sea, said, book, look
+# Topic  2
+# glass, night, house, room, light, summer, airs, bedroom, long, wall
+
 #############################################################
 # SVD and truncate and show variance explained
-lsa = TruncatedSVD(5)
-doc_topic = lsa.fit_transform(doc_word)
-print(sum(lsa.explained_variance_ratio_))
-display_topics(lsa, vectorizer.get_feature_names(), 10)
+# lsa = TruncatedSVD(5)
+# doc_topic = lsa.fit_transform(doc_word)
+# print(sum(lsa.explained_variance_ratio_))
+# display_topics(lsa, vectorizer.get_feature_names(), 10)
 
+import gensim, spacy
+from gensim.utils import lemmatize, simple_preprocess
+import gensim.corpora as corpora
 
+# temp = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in docs]
 
+def process_words(texts, stop_words=stop_words, allowed_postags=['NOUN', 'ADJ', 'ADV']):
+    """Remove Stopwords, Form Bigrams, Trigrams and Lemmatization"""
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    # texts = [bigram_mod[doc] for doc in texts]
+    # texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    texts_out = []
+    nlp = spacy.load('en', disable=['parser', 'ner'])
+    for sent in texts:
+        doc = nlp(" ".join(sent))
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    # remove stopwords once more after lemmatization
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]
+    return texts_out
+
+data_ready = process_words(docs, stop_words_v5)
+
+# Create Dictionary
+id2word = corpora.Dictionary(data_ready)
+
+# Create Corpus: Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in data_ready]
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=3,
+                                           random_state=100,
+                                           passes=10,
+                                           alpha='symmetric',
+                                           iterations=100,
+                                           per_word_topics=True)
+
+lda_model.print_topics()
+
+#######################################################
 # LDA model (need count vectorizer, not tf-idf)
 doc_word = doc_word.transpose()
 # pd.DataFrame(doc_word.toarray(), vectorizer.get_feature_names()).head()
@@ -139,7 +289,7 @@ doc_word = doc_word.transpose()
 corpus = matutils.Sparse2Corpus(doc_word)
 id2word = dict((v, k) for k, v in vectorizer.vocabulary_.items())
 
-lda = models.LdaModel(corpus=corpus, num_topics=5, id2word=id2word, passes=5)
+lda = models.LdaModel(corpus=corpus, num_topics=3, id2word=id2word, passes=5)
 lda.print_topics()
 
 # try clustering on noun phrases
